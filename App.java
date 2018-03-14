12d791013b6d42476a2ef8aa4f74fc4ddda12e14
afterplay <31922021+afterplay@users.noreply.github.com>
2018-03-14 20:57:40 +0800

    Add files via upload

---
+++ App.java
@@ -0,0 +1,13 @@
+package ydManager;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
