f4850d29b6873220d42eb31683ecc10c9cf78828
afterplay <31922021+afterplay@users.noreply.github.com>
2018-03-14 21:26:05 +0800

    /java/test

---
+++ Text.java
@@ -0,0 +1,12 @@
+package animal;
+
+public class Text {
+
+	public static void main(String[] args) {
+		Animal fish = new Fish(2,5);
+		Animal bird = new Bird(4,"ºìÉ«");
+		fish.info();
+		bird.info();
+		}
+
+}
